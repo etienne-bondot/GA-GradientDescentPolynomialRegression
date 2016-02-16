46d5897db41d4da2af41ce279799cb75dc0e4186
Etienne BONDOT <etiennebondot@gmail.com>
2016-02-16 20:41:04 +0000

    Update GA function

--- Population.py
+++ Population.py
@@ -1,4 +1,4 @@
-import random, plotly, math
+import random, plotly, math, time, copy
 from plotly import tools
 from plotly.graph_objs import Scatter, Layout, Marker
 from Gene import *
@@ -11,14 +11,24 @@ class Population:
 
     '''
 
-    def __init__(self, _nb_genes=2, _nb_chromosomes=6, _min=-1.0, _max=1.0):
+    def __init__(self, _nb_genes=1000, _nb_chromosomes=6, _min=-1.0, _max=1.0):
+        self.proba_mutation = 0.001
+        self.proba_crossover = 0.85
         self.nb_genes = _nb_genes
         self.nb_chromosomes = _nb_chromosomes
         self.genes = [Gene(_nb_chromosomes, _min, _max) for _ in range(_nb_genes)]
-        self.info()
 
     def get(self, _ind):
         return self.genes[_ind]
+
+    def len(self):
+        return len(self.genes)
+
+    def copy(self):
+        return copy.copy(self)
+
+    def sorted_genes(self, inputs, targets):
+        self.genes = sorted(self.genes, key=lambda x: x.fitness(inputs, targets))
 
     def gradient_descent(self, inputs, targets, max_iter=1000, alpha=0.001):
         '''
@@ -37,29 +47,46 @@ class Population:
             for j in range(G.len()):
                 newG.set(j, G.get(j) + alpha * G.partial_derivative(inputs, targets))
             G = newG.copy()
-            fitness.append(G.fitness(inputs, targets))
+            fitness.append(G.gd_fitness(inputs, targets))
         # generate a new set of data
         outputs = [G.hypothesis(x) for x in inputs]
         self.chart(inputs, targets, outputs, fitness)
         return G
 
-    def roulette_wheel_selection(self, inputs, targets, max_iter = 1000, alpha = 0.001):
-        self.get(0).fitness(inputs, targets)
-        self.get(1).fitness(inputs, targets)
-        self.crossover()
+    def GA(self, inputs, targets, max_iter = 1000, alpha = 0.001):
+        fitness = []
+        Gs = self.copy()
+        Gs.sorted_genes(inputs, targets)
+        for i in range(max_iter):
+            print '#{}'.format(i),
+            # we keep the best gene and add two others on which we have applied mutation or crossover
+            newGs = list()
+            newGs.append(Gs.get(0).copy())
+            for j in range(Gs.len() / 2):
+                G_1 = Gs.get(j * 2)
+                G_2 = Gs.get(j * 2 + 1)
+                if random.random() < self.proba_crossover:
+                    # print 'crossover'
+                    G_1.chromosomes, G_2.chromosomes = G_1.one_point_crossover(G_2)
+                if random.random() < self.proba_mutation:
+                    # print 'g-1 mutation'
+                    G_1.value_changing_mutation()
+                if random.random() < self.proba_mutation:
+                    # print 'g-2 mutation'
+                    G_2.value_changing_mutation()
+                newGs.extend([G_1, G_2])
+            Gs.genes = list(newGs)
+            Gs.sorted_genes(inputs, targets)
+            _fitness = Gs.get(0).fitness(inputs, targets)
+            print 'F:', _fitness
+            print ''
+            fitness.append(_fitness)
 
-    def crossover(self):
-        rind = self.get(0).len() / 2
-        newG = []
-        for i in range(rind):
-            newG.append(self.get(0).get(i))
-        for i in range(rind, self.get(0).len()):
-            newG.append(self.get(1).get(i))
-        return newG
-
-    def mutate(self, _min=-10.0, _max=10.0):
-        r = random.uniform(_min, _max)
-        rind = random.randrange(0, self.get(0).len())
+        # generate a new set of data
+        print 'best: '
+        Gs.get(0).info()
+        outputs = [Gs.get(0).hypothesis(x) for x in inputs]
+        self.chart(inputs, targets, outputs, fitness)
 
     def select(self, _fitnesses):
         # probability
