fae456c302012a99142a2ba1c03b4dd926d52d7c
Etienne BONDOT <etiennebondot@gmail.com>
2016-02-21 00:19:24 +0000

    add --population and --default

--- main.py
+++ main.py
@@ -1,65 +1,36 @@
 #!/usr/bin/env python
 
-import sys, getopt, random
+import sys, getopt, settings
 from Population import *
-from Gene import *
-
-def display_theta(theta):
-    print '['
-    for t in theta:
-        print ' ', t
-    print ']'
-
-# parse the data file and store the x/y within a list of tuples
-def getData(fname):
-    x = []
-    y = []
-    with open(fname) as f:
-        f.readline()
-        for line in f.readlines():
-            _x, _y = [float(i) for i in line.split()]
-            x.append(_x)
-            y.append(_y)
-    return x, y
-
-def switcher(ind):
-    switch = {
-        0: gradient_descent,
-        1: stochastic_gradient_descent,
-        2: roulette_wheel_selection
-    }
-    return switch.get(ind)
 
 def usage():
     print './main.py -f <datafile> [opts]'
     print '-h, --help : help'
     print '-f, --file= ... : datafile'
+    print '-i, --max-iterations : max iterations'
+    print '-a, --learning-rate : the learning rate'
+    print '-p, --population : the number of genes within the population'
     print '-g, --gradient-descent : gradient descent algorithm'
-    print '-s, --stochastic : stochastic gradient descent algorithm, prior if -g in same time'
+    print '-d, --default : GA with crossovers and mutations'
 
 def main(argv):
-    '''
-
-    Actually, gradient descent algorithm is working with
-    - a polynome of degree 3
-    - a learning rate equals to 0.000000001
-    - a max_iteration equals to 1000
-
-    '''
-
     filename = ''
-    max_iterations = 1000
     alpha = 0.001
+    max_iterations = 3000
+    population = 500
+    chromosomes = 6
+    _min = -1000.0
+    _max = 1000.0
 
     try:
-        opts, args = getopt.getopt(argv, 'hf:gsri:a:', [
+        opts, args = getopt.getopt(argv, 'hf:i:a:p:gd', [
         'help',
         'file=',
-        'radient-descent',
-        'stochastic',
-        'roulette-wheel',
         'max-iterations',
-        'learning-rate'
+        'learning-rate',
+        'population',
+        'gradient-descent',
+        'default'
     ])
     except getopt.GetoptError:
         usage()
@@ -70,17 +41,19 @@ def main(argv):
             sys.exit()
         elif opt in ('-f', '--file'):
             filename = arg
-            inputs, targets = getData(filename)
+            settings.init(filename)
         elif opt in ('-i', '--max-iterations'):
             max_iterations = int(arg)
         elif opt in ('-a', '--learning-rate'):
             alpha = float(arg)
+        elif opt in ('-p', '--population'):
+            population = int(arg)
         elif opt in ('-g', '--gradient-descent'):
-            P = Population(1, 3, -100.0, 100.0)
-            P.gradient_descent(inputs, targets, max_iterations, alpha)
-        elif opt in ('-r', '--roulette-wheel'):
-            P = Population(100, 6, -100.0, 100.0)
-            P.GA(inputs, targets, max_iterations, alpha)
+            P = Population(1, chromosomes, _min, _max)
+            P.gradient_descent(max_iterations, alpha)
+        elif opt in ('-d', '--default'):
+            P = Population(population, chromosomes, _min, _max)
+            P.GA(max_iterations)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
