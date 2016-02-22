f66558b0eb48b9893e62c47e3babc9f798e62f5b
Etienne BONDOT <etiennebondot@gmail.com>
2016-02-22 17:17:04 +0000

    Update logic and parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, getopt, settings
+import sys, getopt, settings, time
 from Population import *
 
 def usage():
@@ -8,29 +8,27 @@ def usage():
     print '-h, --help : help'
     print '-f, --file= ... : datafile'
     print '-i, --max-iterations : max iterations'
-    print '-a, --learning-rate : the learning rate'
     print '-p, --population : the number of genes within the population'
-    print '-g, --gradient-descent : gradient descent algorithm'
-    print '-d, --default : GA with crossovers and mutations'
+    print '-t, --type= [special,one_point_crossover,two_points_crossover] : type of crossover applied on the population'
+
+def update_progress(progress, last_fitness):
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-40s] %d%% - Last fitness: %s" % ('=' * (progress * 40 / 100), progress, last_fitness))
+    sys.stdout.flush()
 
 def main(argv):
     filename = ''
-    alpha = 0.001
+    crossover_methods = []
     max_iterations = 3000
-    population = 500
-    chromosomes = 6
-    _min = -1000.0
-    _max = 1000.0
+    pop_size = 500
 
     try:
-        opts, args = getopt.getopt(argv, 'hf:i:a:p:gd', [
+        opts, args = getopt.getopt(argv, 'hf:i:p:t:', [
         'help',
         'file=',
         'max-iterations',
-        'learning-rate',
         'population',
-        'gradient-descent',
-        'default'
+        'type='
     ])
     except getopt.GetoptError:
         usage()
@@ -44,16 +42,46 @@ def main(argv):
             settings.init(filename)
         elif opt in ('-i', '--max-iterations'):
             max_iterations = int(arg)
-        elif opt in ('-a', '--learning-rate'):
-            alpha = float(arg)
         elif opt in ('-p', '--population'):
-            population = int(arg)
-        elif opt in ('-g', '--gradient-descent'):
-            P = Population(1, chromosomes, _min, _max)
-            P.gradient_descent(max_iterations, alpha)
-        elif opt in ('-d', '--default'):
-            P = Population(population, chromosomes, _min, _max)
-            P.GA(max_iterations)
+            pop_size = int(arg)
+        elif opt in ('-t', '--type'):
+            crossover_methods = arg.split(',')
+
+    start_time = time.time()
+
+    # testing special crossover
+    if not crossover_methods or 'special' in crossover_methods:
+        P = Population(pop_size)
+        for i in range(max_iterations):
+            update_progress(i * 100 / max_iterations, P.best_fitness())
+            P.evolve('special')
+            if P.best_fitness() == 0: break
+
+        P.result('special')
+        print 'processing time: {}'.format(time.time() - start_time)
+
+    # testing one point crossover
+    if not crossover_methods or 'one_point_crossover' in crossover_methods:
+        P = Population(pop_size)
+        for i in range(max_iterations):
+            update_progress(i * 100 / max_iterations, P.best_fitness())
+            P.evolve('one_point_crossover')
+            if P.best_fitness() == 0: break
+
+        P.result('one_point_crossover')
+        print 'processing time: {}'.format(time.time() - start_time)
+
+    # testing one point crossover
+    if not crossover_methods or 'two_points_crossover' in crossover_methods:
+        P = Population(pop_size)
+        for i in range(max_iterations):
+            update_progress(i * 100 / max_iterations, P.best_fitness())
+            P.evolve('two_points_crossover')
+            if P.best_fitness() == 0: break
+
+        P.result('two_points_crossover')
+        print 'processing time: {}'.format(time.time() - start_time)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
