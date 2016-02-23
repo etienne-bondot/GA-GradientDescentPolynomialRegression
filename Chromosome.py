6ee757bdfc2a1299e66cba259190f22728945315
Etienne BONDOT <etiennebondot@gmail.com>
2016-02-23 14:32:28 +0000

    Update crossover logic

--- Chromosome.py
+++ Chromosome.py
@@ -12,6 +12,7 @@ class Chromosome:
         self.crossover_methods = {
             'one_point_crossover': self.one_point_crossover,
             'two_points_crossover': self.two_points_crossover,
+            'average_crossover': self.average_crossover,
             'special': self.crossover
         }
 
@@ -38,35 +39,58 @@ class Chromosome:
     def one_point_crossover(self, parent):
         """
         One point crossover between two parents previously selected.
+        Return the best fitness between the children and the principal parent.
         """
-        pivot = random.randint(0, len(self.genes) - 1)
-        child1 = self.genes[:pivot] + parent.genes[pivot:]
-        child2 = parent.genes[:pivot] + self.genes[pivot:]
-        return (Chromosome(child1), Chromosome(child2))
+        pivot = random.randint(1, len(self.genes) - 1)
+        child1 = self.genes[pivot:] + parent.genes[:pivot]
+        child2 = parent.genes[pivot:] + self.genes[:pivot]
+        child3 = list(self.genes)
+        childs = [Chromosome(child1), Chromosome(child2), Chromosome(child3)]
+        return sorted(childs, key=lambda x: x.fitness)[0]
 
     def two_points_crossover(self, parent):
         """
         Two points crossover between two parents previously selected.
+        Return the best fitness between the children and the principal parent.
         """
-        pivot1 = random.randint(0, len(self.genes) - 1)
-        pivot2 = random.randint(0, len(self.genes) - 1)
-        child1 = parent.chromosomes[0:pivot1] + self.chromosomes[pivot1:pivot2] + parent.chromosomes[pivot2:]
-        child2 = self.chromosomes[0:pivot1] + parent.chromosomes[pivot1:pivot2] + self.chromosomes[pivot2:]
-        return (Chromosome(child1), Chromosome(child2))
+        pivot1 = random.randint(1, len(self.genes) / 2)
+        pivot2 = -1
+        while pivot2 <= pivot1:
+            pivot2 = random.randint(len(self.genes) / 2, len(self.genes) - 1)
+        child1 = parent.genes[pivot2:] + self.genes[pivot1:pivot2] + parent.genes[0:pivot1]
+        child2 = self.genes[pivot2:] + parent.genes[pivot1:pivot2] + self.genes[0:pivot1]
+        child3 = list(self.genes)
+        childs = [Chromosome(child1), Chromosome(child2), Chromosome(child3)]
+        return sorted(childs, key=lambda x: x.fitness)[0]
 
-    def crossover(self, parent):
+    def average_crossover(self, parent):
         """
-        Special crossover between two parents previously selected, looks like a mutation.
+        Average crossover: take the average of the parents genes value to
+        make the childs.
+        Return the best fitness between the children and the principal parent.
         """
         child1 = list(self.genes)
         child2 = list(self.genes)
         child3 = list(self.genes)
         for idx in range(len(self.genes)):
-            child1[idx] = 0.5 * self.genes[idx] + 0.5 * parent.genes[idx]
-            child2[idx] = 1.5 * self.genes[idx] - 0.5 * parent.genes[idx]
-            child3[idx] = -0.5 * self.genes[idx] + 1.5 * parent.genes[idx]
+            child1[idx] = (parent.genes[idx] + self.genes[idx]) / 2.0
+            child2[idx] = (parent.genes[idx] + self.genes[idx]) / 2.0 * -1
         childs = [Chromosome(child1), Chromosome(child2), Chromosome(child3)]
-        return sorted(childs, key=lambda x: x.fitness)[:2]
+        return sorted(childs, key=lambda x: x.fitness)[0]
+
+    def crossover(self, parent):
+        """
+        Special crossover between two parents previously selected, looks like a mutation.
+        Return the best fitness between the children and the principal parent.
+        """
+        child1 = list(self.genes)
+        child2 = list(self.genes)
+        child3 = list(self.genes)
+        for idx in range(len(self.genes)):
+            child1[idx] = random.uniform(-2.0, 2.0) * self.genes[idx] + random.uniform(-2.0, 2.0) * parent.genes[idx]
+            child2[idx] = random.uniform(-2.0, 2.0) * self.genes[idx] - random.uniform(-2.0, 2.0) * parent.genes[idx]
+        childs = [Chromosome(child1), Chromosome(child2), Chromosome(child3)]
+        return sorted(childs, key=lambda x: x.fitness)[0]
 
     def mutate(self, mutation_rate):
         """
